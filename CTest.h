8e5a3df29327d0574dd87f79d92f47a2cfe4de45
Josh Bassett <bassettjosh397@gmail.com>
2025-05-18 11:35:23 +0100

    Added timing functionality

--- CTest.h
+++ CTest.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 /*
 
@@ -32,6 +33,7 @@ typedef struct {
     char* description;  // User provided description
     Status testStatus;  // Status for pass/fail 0 - 3
     char* statusMessage; // Message to display on error
+    double timeTaken;   // Runtime of test
 } Test;
 
 // Main Test
@@ -59,22 +61,39 @@ Test* testInit(const char* description) {
     test->testStatus = IDLE;
 
     test->description = strdup(description); 
-    test->statusMessage = strdup("");        
+    test->statusMessage = strdup("");
+    test->timeTaken = 0.0;        
 
     return test;
 }
 
 // Assert equal A == B
-void assertEqual(CTest* test, int a, int b) {
+void assertEqual(CTest* test, int a, int b, char* description) {
     // Perform out of bounds check
     if (test->elements >= 20) return;
 
     // Create test object
-    char description[] = "TEST";
     Test *newTest = testInit(description);
+
+    // Start test timer
+    clock_t start_time;
+    clock_t end_time;
+    double elapsed_time;
+    start_time = clock(); // Record the start time
+
     // Check value
-    if (a == b) newTest->testStatus = PASSED;
-    else newTest->testStatus = FAILED;
+    if (a == b) {
+        newTest->testStatus = PASSED;
+        asprintf(&(newTest->statusMessage), "%d is equal to %d\n", a, b);
+    } else {
+        newTest->testStatus = FAILED;
+        asprintf(&(newTest->statusMessage), "%d is not equal to %d\n", a, b);
+    }
+
+    // End timer and record time
+    end_time = clock();
+    elapsed_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+    newTest->timeTaken = elapsed_time;
 
     // Add test to main test 
     test->tests[test->elements] = (*newTest);
@@ -98,15 +117,32 @@ void checkStatus(Test* test) {
 
 // Test info display DEBUG function
 void displayInfo(Test* test) {
-    printf("Test ID:\t%s\nDescription:\t%s\nTest Status:\t%u\n", test->id, test->description, test->testStatus);
+    printf("Test ID:\t%s\nDescription:\t%s\nTest Status:\t%u\nMessage:\t%s", test->id, test->description, test->testStatus, test->statusMessage);
     checkStatus(test);
 }
 
+void testResult(Test* test) {
+    checkStatus(test);
+    printf("\nRan test in %f seconds\n%s\n%s", test->timeTaken, test->description, test->statusMessage);
+}
+
 void displayTests(CTest* test) {
-    printf("\nCURRENT TESTS %d\n\n", test->elements);
+    int failures;
+    printf("\nRAN %d TESTS\n\n", test->elements);
     for (int i = 0; i < test->elements; i++) {
-        displayInfo(&test->tests[i]);
+        testResult(&test->tests[i]);
         printf("\n");
+        if (test->tests[i].testStatus == 3) failures++;
+    }
+
+    if (failures > 0) {
+        printf("\033[0;31m"); // red
+        printf("OVERALL TEST FAILURE\n\n");
+        printf("\033[0m");
+    } else  {
+        printf("\033[0;32m"); // green
+        printf("OVERALL TEST PASSED\n\n");
+        printf("\033[0m");
     }
 }
 
@@ -114,9 +150,16 @@ void displayTests(CTest* test) {
 void freeTest(CTest* test) {
     for (int i = 0; i < test->elements; i++) {
         free(test->tests[i].description);
+        free(test->tests[i].statusMessage);
     }
 
     free(test);
+}
+
+// Run tests
+void run(CTest* test) {
+    displayTests(test);
+    freeTest(test);
 }
 
 #endif
--- test.c
+++ test.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "CTest.h"
+#include <time.h>
+
+void test1() {
+    CTest *test = testSetup();
+    int sum = (5 + 5) * 2;
+    assertEqual(test, sum, 20, "Check if sum is equal to 20");
+    assertEqual(test, 10, 10, "Check A == B");
+    run(test);
+}
 
 int main() {
-    CTest *test = testSetup();
-    assertEqual(test, 5, 5);
-    assertEqual(test, 2, 5);
-    displayTests(test);
-    freeTest(test);
+    test1();
 }
